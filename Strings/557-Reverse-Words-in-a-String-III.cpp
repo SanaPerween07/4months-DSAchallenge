fa92819dcc5c663ff72e43bf0dcc34b6ce7982da
Sana Perween <sanahmad15046@gmail.com>
2023-08-31 20:09:14 +0530

    reverse string

---
+++ Strings/557-Reverse-Words-in-a-String-III.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseWords(string str) {
+        int start = 0;
+        int n = str.length();
+        for (int i = 0; i <= n; i++) {
+            if (str[i] == ' ' || i == n) {
+                int end = i - 1;
+                while (start < end) {
+                    swap(str[start], str[end]);
+                    start++;
+                    end--;
+                }
+                start = i + 1;
+            }
+        }
+        return str;
+    }
+};
