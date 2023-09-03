0725edc202e376bfcf985f1494c67d99fec3e80a
Sana Perween <sanahmad15046@gmail.com>
2023-09-03 22:30:15 +0530

    an alternative approach to reverse a string

--- Strings/344-Reverse-String.cpp
+++ Strings/344-Reverse-String.cpp
@@ -7,3 +7,19 @@ public:
         }
     }
 };
+
+
+
+//  alternatively, we can also use two pointer approach
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int start = 0, end = n-1;
+        while(start<end){
+            swap(s[start++], s[end--]);
+        }
+        return s;
+    }
+};
