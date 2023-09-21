433acb290f2aaf571943d61fc553eeba8a988066
Sana Perween <sanahmad15046@gmail.com>
2023-09-21 21:06:57 +0530

    arrays

---
+++ Arrays/1491-Average-Salary-Excluding-the-Minimum-and-Maximum-Salary.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        sort(salary.begin(), salary.end());
+        int n = salary.size();
+        int total_salary = 0;
+        for (int i = 1; i < n - 1; i++) {
+            total_salary += salary[i];
+        }
+        return static_cast<double>(total_salary) / (n - 2);
+    }
+};
