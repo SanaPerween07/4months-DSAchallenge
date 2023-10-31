4a3fdf4408158c7342db13e3fde6f9dc4955302a
Sana Perween <sanahmad15046@gmail.com>
2023-10-31 23:12:44 +0530

    pow

---
+++ maths/50-Pow(x, n).cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        double half = myPow(x, n / 2);
+        double result = half * half;
+
+        if (n % 2 == 1) {
+            result *= x;
+        }
+
+        return result;
+    }
+};
